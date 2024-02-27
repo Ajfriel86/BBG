ef0343e62bc3e146f29d7f46cd855fd3f6ed1f1c
Angus Friel <angus.friel.1986@gmail.com>
2024-02-27 09:17:03 +0000

    Fixing static files

--- BBG/settings.py
+++ BBG/settings.py
@@ -167,7 +167,7 @@ cloudinary.config(
 
 # Static and media files configuration, including URLs and storage backends
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
